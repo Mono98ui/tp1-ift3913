72a1644fa26c0ae7e1a320906eabe965b316e502
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2023-09-26 10:08:40 -0400

    ajout des asserts

--- partie 2/tassert/src/main/java/org/example/Main.java
+++ partie 2/tassert/src/main/java/org/example/Main.java
@@ -9,12 +9,15 @@ import java.util.regex.Pattern;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    
     public static void main(String[] args) throws IOException {
         File fileToRead = new File(args[0]);
         BufferedReader br = new BufferedReader(new FileReader(fileToRead));
         String line = br.readLine();
 
-        Pattern pCom = Pattern.compile("^(assert|fail)");
+        Pattern pCom = Pattern.compile("^(assertArrayEquals|assertEquals|assertFalse" +
+                "|assertNotEquals|assertNotNull|assertNotSame|assertNull|assertSame|" +
+                "assertThat|assertThrows|assertTrue|fail)\\(.*\\)$");
         int nbrAssert = 0;
 
         while (line != null) {
