358feae70c53cec690883fb1401494faea3a97fa
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2023-09-29 13:57:18 -0400

    Iteration recursive dun dossier

--- partie 4/tropcomp/src/main/java/org/example/Main.java
+++ partie 4/tropcomp/src/main/java/org/example/Main.java
@@ -1,24 +1,35 @@
 package org.example;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
         File dirName = new File(args[0]);
         File[] dir = dirName.listFiles();
         System.out.println(args[1]);
         int seuil = Integer.parseInt(args[1]);
-        int resultAnalyse = 0;
+
+        resursiveListDir(dir,seuil);
+
+    }
+
+    public static void resursiveListDir(File[] dir, int seuil){
         for (File f:
-             dir) {
-            System.out.println(f.getAbsolutePath());
-            System.out.println(f.isDirectory());
-            if(seuil < resultAnalyse){
-                System.out.println("HelloWorld");
+                dir) {
+            Pattern comp = Pattern.compile(".java$");
+            if(comp.matcher(f.getName()).find()){
+                //code a ajouter
+                System.out.println(f.getAbsolutePath());
+                System.out.println(f.isDirectory());
+                System.out.println(f.getName());
+            }
+            if(f.isDirectory()){
+                resursiveListDir(f.listFiles(), seuil);
             }
         }
-
     }
 }
