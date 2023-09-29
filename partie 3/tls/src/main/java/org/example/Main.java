66bf38cff2c4af169b8782f97d49d86a510656fb
unknown <tbenakezouh@yahoo.ca>
2023-09-29 11:23:43 -0400

    6 valeurs des colonnes du fichiers CSV
    Logique de base. Fonctionnalité à confirmer plus tard.

--- partie 3/tls/src/main/java/org/example/Main.java
+++ partie 3/tls/src/main/java/org/example/Main.java
@@ -1,18 +1,79 @@
 package org.example;
 
 import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int tlocOutput = tloc(args);
-        int tassertOutput = tassert(args);
-        System.out.println(tlocOutput);
-        System.out.println(tassertOutput);
+        File fileToRead = new File(args[0]);
+
+        //chemin du fichier
+        String filePath = fileToRead.getPath();
+
+        Scanner scanner = new Scanner(fileToRead);
+        String targetWord = "package";
+        int lineNumber = 0;
+
+        //nom du paquet
+        while (scanner.hasNextLine()) {
+            lineNumber++;
+            String line = scanner.nextLine();
+            if (line.contains(targetWord)) {
+                int index = line.indexOf(targetWord);
+                String packageName = line.substring(index + targetWord.length()).trim();
+                System.out.println("Nom du paquet: " + packageName);
+            }
+        }
+
+        scanner.close();
+
+        //nom de la classe
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String className = getClassName(line);
+                if (className != null) {
+                    System.out.println("Class Name: " + className);
+                    break; // Assuming there's only one class per file
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Alternative plus simple, mais pas nécessairement valide
+        //String fileName = fileToRead.getName();
+
+        //tloc de la classe
+        int tlocOutput = tloc(fileToRead);
+        System.out.println("tloc de la classe: " + tlocOutput);
+
+        //tassert de la classe
+        int tassertOutput = tassert(fileToRead);
+        System.out.println("tassert de la classe: " +tassertOutput);
+
+        //tcmp de la classe
+        double tcmp = tlocOutput/tassertOutput;
+
     }
 
-    static int tloc(String[] args) throws IOException {
-        File fileToRead = new File(args[0]);
+    static String getClassName(String line) {
+        // Ignore les mentions de class dans les commentaires
+        if (line.trim().startsWith("//")) {
+            return null;
+        }
+
+        Pattern pattern = Pattern.compile("class\\s+(\\w+)");
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    static int tloc(File fileToRead) throws IOException {
         //System.out.println(fileToRead.getAbsolutePath());
         BufferedReader br = new BufferedReader(new FileReader(fileToRead));
         String line = br.readLine();
@@ -45,8 +106,7 @@ public class Main {
         return (nbrLine - nbrComment);
     }
 
-    static int tassert(String[] args) throws IOException {
-        File fileToRead = new File(args[0]);
+    static int tassert(File fileToRead) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(fileToRead));
         String line = br.readLine();
 
