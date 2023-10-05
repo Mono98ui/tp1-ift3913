728230da15869e8e021485b52cf64ec84ab06750
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2023-10-05 13:20:29 -0400

    ajout recurssion des dossiers de partie 4

--- partie 3/tls/src/main/java/org/example/Main.java
+++ partie 3/tls/src/main/java/org/example/Main.java
@@ -1,90 +1,90 @@
 package org.example;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.opencsv.CSVWriter;
 
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws IOException, ArithmeticException {
-        File fileToRead = null;
-        if(args.length >= 3){ //Chemin du fichier CSV passé en paramètre
-            fileToRead = new File(args[2]);
 
-        } else { //Fichier d'entrée passé en paramètre seulement
-            fileToRead = new File(args[0]);
+    static String csvName = "";
+    static String pathFolder = "";
+    static boolean isCsv = false;
+
+    static ArrayList<Node> arr;
+
+    static class Node {
+        String filepath = "";
+        int tloc = 0;
+
+        double tcmp = 0;
+
+        String[] content;
+
+        public Node(String filepath, int tloc, double tcmp) {
+            this.filepath = filepath;
+            this.tloc = tloc;
+            this.tcmp = tcmp;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args[0].contains("-o")) {
+            csvName = args[1];
+            pathFolder = args[2];
+            isCsv = true;
+        } else {
+            pathFolder = args[0];
         }
 
-        //chemin du fichier
-        String filePath = fileToRead.getPath();
+        arr = new ArrayList<Node>();
 
-        Scanner scanner = new Scanner(fileToRead);
-        String targetWord = "package";
-        int lineNumber = 0;
-
-        //nom du paquet
-        String packageName = null;
-
-        while (scanner.hasNextLine()) {
-            lineNumber++;
-            String line = scanner.nextLine();
-            if (line.contains(targetWord)) {
-                int index = line.indexOf(targetWord);
-                packageName = line.substring(index + targetWord.length()).trim();
-                System.out.println("Nom du paquet: " + packageName);
-            }
-        }
-        scanner.close();
-
-        //nom de la classe
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
-        String line, className = null;
-
-        while ((line = br.readLine()) != null) {
-            className = getClassName(line);
-            if (className != null) {
-                System.out.println("Class Name: " + className);
-                break;
-            }
-        }
-
-        //Alternative plus simple, mais pas nécessairement valide
-        //String fileName = fileToRead.getName();
-
-        //tloc de la classe
-        int tlocOutput = tloc(fileToRead);
-        System.out.println("tloc de la classe: " + tlocOutput);
-
-        //tassert de la classe
-        int tassertOutput = tassert(fileToRead);
-        System.out.println("tassert de la classe: " + tassertOutput);
-
-        //tcmp de la classe
-        double tcmpOutput = 0;
-
-        if (tlocOutput != 0 && tassertOutput != 0) {
-            tcmpOutput = (double) (tlocOutput / tassertOutput);
-        }
+        resursiveListDir(new File(pathFolder).listFiles());
 
 
-        String currentLine[] = {filePath, packageName, className, Integer.toString(tlocOutput),
-                Integer.toString(tassertOutput), Double.toString(tcmpOutput)};
 
-        if (args.length >= 3){ //Écriture CSV
-            String CSVPath = args[1];
+        if (isCsv) {
+            String CSVPath = csvName;
             File file = new File(CSVPath);
             CSVWriter writer = new CSVWriter(new FileWriter(CSVPath));
+            for (int i = 0; i < arr.size() ; i++) {
+                System.out.println(arr.get(i).filepath);
+                File fileToRead = new File(arr.get(i).filepath);
+                writer.writeNext(arr.get(i).content);
+                writer.flush();
+                System.out.println("Données CSV insérées.");
 
-            writer.writeNext(currentLine);
-            writer.flush();
-            System.out.println("Données CSV insérées.");
-        } else { //Publication sur le terminal
-            System.out.println(Arrays.toString(currentLine));
+            }
+        }else{
+            for (int i = 0; i < arr.size() ; i++) {
+                System.out.println(arr.get(i).filepath);
+                File fileToRead = new File(arr.get(i).filepath);
+                System.out.println(Arrays.toString(arr.get(i).content));
+            }
         }
 
+
+
+    }
+
+    public static void resursiveListDir(File[] dir) throws IOException {
+        for (File f :
+                dir) {
+            Pattern comp = Pattern.compile(".java$");
+            if (comp.matcher(f.getName()).find()) {
+                System.out.println(f.getAbsolutePath());
+                System.out.println(f.isDirectory());
+                System.out.println(f.getName());
+                tls(f);
+            }
+            if (f.isDirectory()) {
+                resursiveListDir(f.listFiles());
+            }
+        }
     }
 
     static String getClassName(String line) {
@@ -153,6 +153,64 @@ public class Main {
         return (nbrAssert);
     }
 
+    static void tls(File fileToRead) throws IOException {
+
+
+        //chemin du fichier
+        String filePath = fileToRead.getPath();
+
+        Scanner scanner = new Scanner(fileToRead);
+        String targetWord = "package";
+        int lineNumber = 0;
+
+        //nom du paquet
+        String packageName = null;
+
+        while (scanner.hasNextLine()) {
+            lineNumber++;
+            String line = scanner.nextLine();
+            if (line.contains(targetWord)) {
+                int index = line.indexOf(targetWord);
+                packageName = line.substring(index + targetWord.length()).trim();
+                System.out.println("Nom du paquet: " + packageName);
+            }
+        }
+        scanner.close();
+
+        //nom de la classe
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        String line, className = null;
+
+        while ((line = br.readLine()) != null) {
+            className = getClassName(line);
+            if (className != null) {
+                System.out.println("Class Name: " + className);
+                break;
+            }
+        }
+
+        //Alternative plus simple, mais pas nécessairement valide
+        //String fileName = fileToRead.getName();
+
+        //tloc de la classe
+        int tlocOutput = tloc(fileToRead);
+        System.out.println("tloc de la classe: " + tlocOutput);
+
+        //tassert de la classe
+        int tassertOutput = tassert(fileToRead);
+        System.out.println("tassert de la classe: " + tassertOutput);
+
+        //tcmp de la classe
+        double tcmpOutput = 0;
+
+        if (tlocOutput != 0 && tassertOutput != 0) {
+            tcmpOutput = (double) (tlocOutput / tassertOutput);
+        }
+
+        String currentLine[] = {filePath, packageName, className, Integer.toString(tlocOutput),
+                Integer.toString(tassertOutput), Double.toString(tcmpOutput)};
+        Node newNode = new Node(fileToRead.getAbsolutePath(), tlocOutput, tcmpOutput);
+        newNode.content = currentLine;
+        arr.add(newNode);
+    }
 }
-
-
